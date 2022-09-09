864c51fbf9681ad8993fb2e3eb9c3550ebf1422f
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2022-09-10 00:55:09 +0430

    Progressing On POST

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react"
-import { getList, setItem } from '../sevices/APIs'
+import { getList, setItem  } from '../sevices/APIs'
+import axios from "axios";
 
 
 export default function App () {
-    const [titleInput, setTitleInput] = useState('');
     const [list, setList] = useState([]);
+    const [post, setPost] = useState([])
+    
     useEffect(() => {
         let mounted = true;
         getList()
@@ -16,11 +18,26 @@ export default function App () {
             return () => mounted = false;
     }, [])
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setItem(titleInput)
-    } 
+    const token = '6ae33a85855d94d4879dace98654d2a54f81bf335a3440121e6caff536e718ad';
     
+    function SubmitFunction(e) {
+        e.preventDefault()
+        console.log("Hello")
+        setItem(post)
+    }
+    
+    useEffect(() => {
+        axios
+            .post("https://gorest.co.in/public/v2/posts", {
+                title : "Wooww I done it now"
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+    }, [])
+    
+
 
 
     return (
@@ -32,13 +49,13 @@ export default function App () {
                     {list.map(item => <h3 key={item.title}><span className="bold">Title:</span> {item.title} <p>ID: {item.id}</p> <hr /> {item.body}</h3>)}
                     <hr />
                 </div>
-                <form className="form" onSubmit={handleSubmit}>
+                <form className="form" >
                     <label>
                         <p>Adding New Item: </p>
-                        <p>Title: </p><input type="text" onChange={event => setTitleInput(event.target.value)} value={titleInput} className="titletext"/>
+                        <p>Title: </p><input type="text" onChange={event => setPost(event.target.value)} className="titletext" value={post}/>
                         <p>Content: </p>
                         <textarea type="text" className="bodytext"/>
-                        <button type="submit">Submit</button>
+                        <button type="submit" onClick={SubmitFunction}>Submit</button>
                     </label>
                 </form>
         </div>
